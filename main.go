eb52a819f52f3919c1d470b202edd8d3627a704b
lpoliveiraneto <leonidespires@gmail.com>
2025-06-10 19:59:33 -0300

    Continuação

--- main.go
+++ main.go
@@ -12,10 +12,23 @@ type ContaCorrente struct {
 	saldo       float64
 }
 
+// utilizando no struct no main
 func main() {
+
 	conta := ContaCorrente{titular: "Goku", agencia: 111,
 		numeroConta: 123456, saldo: 142.50}
-	fmt.Println(conta)
-	conta2 := ContaCorrente{"Neto", 111, 123456, 150.00}
-	fmt.Println("Essa é outra conta: ", conta2)
+	conta2 := ContaCorrente{titular: "Goku", agencia: 111,
+		numeroConta: 123456, saldo: 142.50}
+	//como se fosse o equals, pois compara o conteúdo.
+	fmt.Println(conta == conta2)
+	// conta2 := ContaCorrente{"Neto", 111, 123456, 150.00}
+	// fmt.Println("Essa é outra conta: ", conta2)
+
+	// //exemplo de como utilizar ponteiros
+	// var contaDoNeto *ContaCorrente
+	// contaDoNeto = new(ContaCorrente)
+	// contaDoNeto.titular = "Neto"
+
+	// fmt.Println(*contaDoNeto)
+
 }
